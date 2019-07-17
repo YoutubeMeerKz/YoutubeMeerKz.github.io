ee9a437f3e08a3c2657f6ea377fd2b5afdbd55ef
Thiago <48030640+YoutubeMeerKz@users.noreply.github.com>
2019-07-17 16:52:24 -0300

    Add files via upload

---
+++ calculadora/JS/meujavascript.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Parágrafo mudado.";}
+
+window.alert("este é um alerta");
+console.log("só apareço no f12");
+
+    function minhafunção2(){
+        if (document.getElementById("demo1").innerHTML.length === 0)
+        document.getElementById("demo1").innerHTML = "olá Dolly!";
+
+    document.getElementById("demo2").innerHTML =
+"Olá brasil!";
+
+document.getElementById("demo1").innerHTML = "";
+
+document.getElementById("dem1").innerHTML = "";
+}
